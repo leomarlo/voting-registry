a3708668a80d800a413a000ee0679ef3f56f03ad
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-05-02 14:50:31 +0200

    some beautifications of the voting Registry interface

--- contracts/registry/IVotingRegistry.sol
+++ contracts/registry/IVotingRegistry.sol
@@ -11,10 +11,10 @@ address constant REGISTRY = 0x1460ce805f5E6dc4Afd65960c7a8DD23bB3EeF6A;
 
 interface IVotingRegistry {
 
-    function register(bytes8) external returns(uint256 registrationIndex);
-    function isRegistered(address) view external returns(bool registrationFlag);
+    function register(bytes8 categoryId) external returns(uint256 registrationIndex);
+    function isRegistered(address voteContract) external view returns(bool registrationFlag);
 
-    function addCategoryToRegistration(bytes8) external;
-    function isRegisteredCategory(bytes8) view external returns(bool registrationFlag);
+    function addCategoryToRegistration(bytes8 categoryId) external;
+    function isRegisteredCategory(bytes8 categoryId) external view returns(bool registrationFlag);
 
 }
